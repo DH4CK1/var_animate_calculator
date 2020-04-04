3065edb1775007cccdf5bbd1ebe9d0b7b823426c
Đ`HACK <56754416+DH4CK1@users.noreply.github.com>
2020-04-04 16:50:30 +0700

    Add files via upload

---
+++ calTes.py
@@ -0,0 +1,24 @@
+#!/bin/python2.7
+import var_animate
+
+animvar = var_animate.animvar()
+animinput = var_animate.animinput()
+banner = var_animate.banner('Calculator','Desta','1.0')
+
+print(banner)
+no1 = int(animinput.ask('Nomer1'))
+opt = animinput.ask('Operator[+/-*]')
+no2 = int(animinput.ask('Nomer2'))
+
+if opt == "+":
+   hasil = no1+no2
+   print(animvar.true('Hasil: {}').format(hasil))
+elif opt == '-':
+   hasil = no1-no2
+   print(animvar.true('Hasil: {}').format(hasil))
+elif opt == '*':
+   hasil = no1*no2
+   print(animvar.true('Hasil: {}').format(hasil))
+elif opt == '/':
+   hasil = no1/no2
+   print(animvar.true('Hasil: {}').format(hasil))
